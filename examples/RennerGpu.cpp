36589b4976866b9be8d5b9ceee2b59cf2997d747
Vanzo Daniel <vanzo_dan@cam16.grid.metz.supelec.fr>
2018-04-11 17:07:43 +0200

    Test RennerGpu wrong result because of hash colision

--- examples/RennerGpu.cpp
+++ examples/RennerGpu.cpp
@@ -43,7 +43,7 @@ google::dense_hash_map<PTransf16, std::pair<PTransf16, int>,
 unordered_map<PTransf16, std::pair<PTransf16, int>> elems;
 #endif
 
-void hash_cpu(const uint32_t* __restrict__ x, uint64_t* hashed, const size_t size, const size_t nb_vect) {
+void hash_cpu(const uint32_t* __restrict__ x, uint64_t* hashed, const int size, const int nb_vect) {
   const uint64_t prime = 0x9e3779b97f4a7bb9;
   uint64_t tmp;
   __int128 v0;
@@ -61,76 +61,230 @@ void hash_cpu(const uint32_t* __restrict__ x, uint64_t* hashed, const size_t siz
 
 
 
-int main() {
-  //~ int lg = 0;
-  using namespace std::chrono;
-  size_t size = 50000;
-  int nb_vect = 5;
-  int block_size = 32;
-  int nb_hash = 100;
-  int size_start = 16;
-  int size_end = 100000;
-  int size_nb = 200;
-  int vect_start = 1;
-  int vect_end = 10000;
-  int vect_nb = 200;
-  int coefPerThread;
-  int kernel_num = 3;
+//~ int main() {
+  //~ using namespace std::chrono;
+  //~ int size = 50000;
+  //~ int nb_vect = 5;
+  //~ int block_size = 32;
+  //~ int nb_hash = 100;
+  //~ int size_start = 2000;
+  //~ int size_end = 250000;
+  //~ int size_nb = 5;
+  //~ int vect_start = 200;
+  //~ int vect_end = 300;
+  //~ int vect_nb = 2;
+  //~ int coefPerThread;
+  //~ int kernel_num = 3;
   
-  double timeGpu, timeCpu; 
+  //~ double timeGpu, timeCpu; 
   
-  string file_name = to_string(size_end) + to_string(size_nb) + to_string(vect_end) + to_string(vect_nb) + to_string(nb_hash) + to_string(kernel_num);
-  
-  for(nb_vect = vect_start; nb_vect<vect_end; nb_vect = ceil(nb_vect*pow(vect_end/vect_start, 1./vect_nb)) ){
-    printf("nb_vect : %d\n", nb_vect);
+  //~ string file_name = to_string(size_end) + to_string(size_nb) + to_string(vect_end) + to_string(vect_nb) + to_string(nb_hash) + to_string(kernel_num);
+  //~ printf("kernel_num : %d\n", kernel_num);
+  //~ for(nb_vect = vect_start; nb_vect<vect_end; nb_vect = ceil(nb_vect*pow(vect_end/vect_start, 1./vect_nb)) ){
+    //~ printf("nb_vect : %d\n", nb_vect);
 
-    for(size = size_start; size<size_end; size = ceil(size *pow(size_end/size_start, 1./size_nb))){
-      printf("size : %d\n", size);
-      uint32_t* gen = (uint32_t*)malloc(size * nb_vect * sizeof(uint32_t));    
-      uint64_t* hashed = (uint64_t*)malloc(nb_vect * sizeof(uint64_t));     
+    //~ for(size = size_start; size<size_end; size = ceil(size *pow(size_end/size_start, 1./size_nb))){
+      //~ printf("size : %d\n", size);
+      //~ uint32_t* gen = (uint32_t*)malloc(size * nb_vect * sizeof(uint32_t));    
+      //~ uint64_t* hashed = (uint64_t*)malloc(nb_vect * sizeof(uint64_t));     
        
-      for(int i=0; i<nb_vect; i++)
-        for(int j=0; j<size; j++)
-          gen[i*size + j] = (uint32_t)(j+i);  
+      //~ for(int i=0; i<nb_vect; i++)
+        //~ for(int j=0; j<size; j++)
+          //~ gen[i*size + j] = (uint32_t)(j+i);
   
-      for(block_size = 4; block_size<=5; block_size*=2){
-        // GPU ############################
+      //~ for(block_size = 4; block_size<=5; block_size*=2){
+        //~ // GPU ############################
         //~ auto tstartGpu = high_resolution_clock::now();
           //~ for(int j=0; j<nb_hash; j++)
-            //~ hash_gpu(gen, block_size, hashed, size, nb_vect);
+            //~ hash_gpu(gen, block_size, hashed, size, nb_vect, kernel_num);
         //~ auto tfinGpu = high_resolution_clock::now();
         //~ auto tmGpu = duration_cast<duration<double>>(tfinGpu - tstartGpu);
         //~ timeGpu = tmGpu.count()/nb_hash*1e6;
         //~ for(int i=0; i<nb_vect; i++)
           //~ printf("Hash GPU : %lu, index : %lu\n", hashed[i], hashed[i]%100000);
-        // CPU ############################
-        auto tstartCpu = high_resolution_clock::now();
-          for(int j=0; j<nb_hash; j++)
-            hash_cpu(gen, hashed, size, nb_vect);
-        auto tfinCpu = high_resolution_clock::now();
-        auto tmCpu = duration_cast<duration<double>>(tfinCpu - tstartCpu);
-        timeCpu = tmCpu.count()/nb_hash*1e6;
-        for(int i=0; i<nb_vect; i++)
-          printf("Hash CPU : %lu, index : %lu\n", hashed[i], hashed[i]%100000);
+        //~ // CPU ############################
+        //~ auto tstartCpu = high_resolution_clock::now();
+          //~ for(int j=0; j<nb_hash; j++)
+            //~ hash_cpu(gen, hashed, size, nb_vect);
+        //~ auto tfinCpu = high_resolution_clock::now();
+        //~ auto tmCpu = duration_cast<duration<double>>(tfinCpu - tstartCpu);
+        //~ timeCpu = tmCpu.count()/nb_hash*1e6;
+        //~ for(int i=0; i<nb_vect; i++)
+          //~ printf("Hash CPU : %lu, index : %lu\n", hashed[i], hashed[i]%100000);
          
-        if(kernel_num == 1)
-          coefPerThread = (size+block_size-1) / block_size;
-        else if(kernel_num == 2)
-          coefPerThread = (size+32-1) / 32;
+        //~ if(kernel_num == 1)
+          //~ coefPerThread = (size+block_size-1) / block_size;
+        //~ else if(kernel_num > 1)
+          //~ coefPerThread = (size+32-1) / 32;
         //~ printf("coefPerThread : %d, nb vect : %d\n", coefPerThread, nb_vect);
         //~ printf("Speedup : %f\n", timeCpu/timeGpu);
         //~ printf("Block size : %d, size : %lu, time : %.3f us, time/vect : %.3f us\n", block_size, size, timeGpu, timeGpu/nb_vect);
         //~ printf("Block size : , size : %lu, time : %.3f us, time/vect : %.3f us\n", size, timeCpu, timeCpu/nb_vect);
         
-        //~ write_hash(block_size, size, nb_vect, hashed, timeGPU, file_name);
-        write_hash(1, size, nb_vect, hashed, timeCpu, file_name);
-      }
+        //~ write_hash(block_size, size, nb_vect, hashed, timeGpu, file_name);
+        //~ write_hash(1, size, nb_vect, hashed, timeCpu, file_name);
+      //~ }
       //~ printf("\n");
   
-      free(gen);
-      free(hashed);
-    }
-  }
-  printf("end\n");
+      //~ free(gen);
+      //~ free(hashed);
+    //~ }
+  //~ }
+  //~ printf("end\n");
 
+//~ }
+
+
+
+#define HASH_SIZE 10000
+#define NODE 50
+
+void print_word(std::array<int, NODE> word);
+
+
+struct eqstr
+{
+  bool operator()(const std::pair<uint64_t, std::array<int, NODE>> pair1, const std::pair<uint64_t, std::array<int, NODE>> pair2) const
+  {
+    return pair1.first == pair2.first;
+    //~ return equal_gpu(&(pair1.second[0]), &(pair2.second[0]), d_gen, block_size, size, size_word, nb_gen);
+  }
+};
+
+struct hash_gpu_class
+{
+  bool operator()(const std::pair<uint64_t, std::array<int, NODE>> pair) const
+  {
+    return pair.first%HASH_SIZE;
+  }
+};
+
+
+int main() {
+  using namespace std::chrono;
+  const int size = 16;
+  int block_size = 4;
+  const int nb_gen = 4;
+  //~ const int node = 50;
+
+  google::dense_hash_map< std::pair< uint64_t, std::array<int, NODE> >, std::array<int, NODE>, hash_gpu_class, eqstr> elems;
+
+  uint32_t* gen = (uint32_t*)malloc(size*nb_gen * sizeof(uint32_t));
+  for(int i=0; i<size*nb_gen; i++){
+    gen[i] = i%size;
+  }
+  gen[9] = 10;
+  gen[10] = 9;
+  gen[size + 10] = 11;
+  gen[size + 11] = 10;
+  gen[2*size + 3] = 4;
+  gen[2*size + 4] = 3;
+  gen[3*size + 2] = 3;
+  gen[3*size + 3] = 2;
+  //~ gen[4*size + 1] = 2;
+  //~ gen[4*size + 2] = 1;
+  uint32_t* d_gen;
+  malloc_gen(&d_gen, gen, size, nb_gen);
+
+
+
+  vector< std::array<int, NODE> > todo, newtodo;
+  std::array<int, NODE> id_word;
+  id_word.fill(-1);
+  todo.push_back(id_word);
+
+  elems.set_empty_key(std::make_pair (-1, id_word));
+
+  uint64_t hashedId;
+  hash_id_gpu(&hashedId, block_size, size);  
+  elems.insert({ {hashedId, id_word}, id_word});
+  
+  
+  for(int i=0; i<NODE; i++){
+    newtodo.clear();
+    uint64_t* hashed = (uint64_t*)malloc(todo.size()*nb_gen * sizeof(uint64_t)); // Todo use of vector
+    hpcombi_gpu(&(todo[0][0]), d_gen, hashed, block_size, size, NODE, todo.size(), nb_gen);
+    
+    for(int j=0; j<todo.size()*nb_gen; j++){
+      std::array<int, NODE> newWord = todo[j/nb_gen];        
+      newWord[i] = gen[j%nb_gen];
+      //~ print_word(newWord);
+      if(elems.insert({ {hashed[j], newWord}, newWord}).second){
+        newtodo.push_back(newWord);
+      //~ cout << "new   " << j << "  " << hashed[j]%HASH_SIZE << endl;
+      }
+      else{
+        //~ cout << "old  " << j << "  " << hashed[j]%HASH_SIZE << endl;
+        //~ print_word(elems[hashed[j]]);
+      }
+    }
+
+    free(hashed);
+    std::swap(todo, newtodo);
+    cout << i << ", todo = " << todo.size() << ", elems = " << elems.size()
+         << ", #Bucks = " << elems.bucket_count() << endl;
+         
+         
+    //~ for(int i=0; i<todo.size()*NODE; i++){
+      //~ printf("%d|", *(&(todo[0][0])+i));
+      //~ if(i%NODE==NODE-1)
+      //~ printf("\n");
+    //~ }
+    //~ printf("\n");
+    
+    if(todo.size() == 0)
+      break;
+  }
+  cout << "elems =  " << elems.size() << endl;
+  free(gen);
+  free_gen(&d_gen);
 }
+
+
+
+
+
+void print_word(std::array<int, NODE> word){
+  for(int i=0; i<NODE; i++)
+    printf("%d|", word[i]);
+  printf("\n");
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
