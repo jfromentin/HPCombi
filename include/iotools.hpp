06223c3cb4498983c8c023b684b9df698096a455
Daniel Vanzo <daniel.vanzo@lri.fr>
2018-05-15 10:34:38 +0200

    Read function for gen files

--- include/iotools.hpp
+++ include/iotools.hpp
@@ -7,3 +7,4 @@
 
 void write_hash(int block_size,  const size_t size, const size_t nb_vect, uint64_t* hashed, double time, std::string file_name);
 void write_renner(const int size, const int8_t nb_gen, const int count, double time);
+void readRenner(std::string file_name, uint32_t** gen, int* size, int8_t* nb_gen);
--- src/iotools.cpp
+++ src/iotools.cpp
@@ -1,5 +1,7 @@
 #ifndef HPCOMBI_IOTOOLS_HPP_INCLUDED
 #include <iotools.hpp>
+#include <sstream>
+#include <string>
 
 void write_hash(int block_size,  const int size, const int nb_vect, uint64_t* hashed, double time, std::string file_name){
 	file_name = "examples/outData/" + file_name + ".data";
@@ -19,5 +21,36 @@ void write_renner(const int size, const int8_t nb_gen, const int count, double t
 	myfile << size << ";" << time << std::endl;
 	myfile.close();	
 }
+
+
+void readRenner(std::string file_name, uint32_t** gen, int* size, int8_t* nb_gen){
+	file_name = "../data/" + file_name;
+	std::ifstream infile(file_name);
+	std::string line;
+	std::getline(infile, line);
+	std::getline(infile, line);
+	*nb_gen = std::stoi(line);
+	if(*nb_gen <= 0){
+		printf("nb_gen is not valid : %d", *nb_gen);
+		exit(1);
+	}
+	std::getline(infile, line);
+	*size = std::stoi(line);	
+	if(*size <= 0){
+		printf("Size is not valid : %d", *size);
+		exit(1);
+	}
 	
+	(*gen) = (uint32_t*)malloc((*size)*(*nb_gen) * sizeof(uint32_t));
+	std::cout << "Reading file " << file_name << ", size : " << *size << ", nb_gen : " << (int)*nb_gen << std::endl;;
+		
+	for(int i=0; i<*nb_gen; i++){
+		std::getline(infile, line);
+	    std::istringstream iss(line);
+	    for(int j=0; j<*size; j++){
+			iss >> (*gen)[j + i*(*size)];
+		}
+	}
+}
+
 #endif  // HPCOMBI_IOTOOLS_HPP_INCLUDED
