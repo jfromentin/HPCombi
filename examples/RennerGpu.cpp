730c6ffc83f797c0671939b76122d207d2f77e46
Daniel Vanzo <daniel.vanzo@lri.fr>
2018-05-23 10:56:38 +0200

    Add number of generators in data files, adpatedfilename reading parameter

--- examples/RennerGpu.cpp
+++ examples/RennerGpu.cpp
@@ -65,94 +65,95 @@ int main(int argc, char* argv[]){
 //~ for(size=100000; size<100001; size *=1.08){
 	uint32_t* gen;
   int inParam = 2;
+  std::string fileName;
   if(argc > 1){
-    inParam = *argv[1] - '0';
-    if(inParam > 7 || inParam < 2)
-      printf("Parameter must be in [2,7], setting it to 2.\n");
+    fileName = std::string(argv[1]);
   }
-	std::string fileName = "RenA" + std::to_string(inParam) + ".txt";
-    readRenner(fileName, &gen, &size, &nb_gen);
-    printf("Size : %d\n", size);
+  else
+    fileName = "RenA2";
+	fileName = fileName + ".txt";
+  readRenner(fileName, &gen, &size, &nb_gen);
+  printf("Size : %d\n", size);
 
-    printf("\n");
-  
-    uint32_t* d_gen;
-    int8_t* d_words;
-    malloc_gen(d_gen, gen, size, nb_gen);
-    malloc_words(d_words, NODE);
-  
-    //~ google::dense_hash_map< Key, std::array<int8_t, NODE>, hash_gpu_class, eqTrans> elems(25000);
+  printf("\n");
 
+  uint32_t* d_gen;
+  int8_t* d_words;
+  malloc_gen(d_gen, gen, size, nb_gen);
+  malloc_words(d_words, NODE);
+
+  //~ google::dense_hash_map< Key, std::array<int8_t, NODE>, hash_gpu_class, eqTrans> elems(25000);
+
+
+  Vector_cpugpu<int8_t> todo(pow(2, 12));
+  Vector_cpugpu<int8_t> newtodo(pow(2, 12));
+  Vector_gpu<uint32_t> workSpace(pow(2, 12));
+  Vector_cpugpu<uint64_t> hashed(pow(2, 12));
+  Vector_cpugpu<int> equal(1);
+  equal.push_back(0);
+  std::array<int8_t, NODE> empty_word;
+  empty_word.fill(-10);
   
-    Vector_cpugpu<int8_t> todo(pow(2, 12));
-    Vector_cpugpu<int8_t> newtodo(pow(2, 12));
-    Vector_gpu<uint32_t> workSpace(pow(2, 12));
-    Vector_cpugpu<uint64_t> hashed(pow(2, 12));
-    Vector_cpugpu<int> equal(1);
-    equal.push_back(0);
-    std::array<int8_t, NODE> empty_word;
-    empty_word.fill(-10);
+  Key empty_key(UINT64_MAX, empty_word);
+
+  hash_gpu_class hashG;
+  eqTrans equalG(d_gen, d_words, size, nb_gen, equal);
+  google::dense_hash_map< Key, std::array<int8_t, NODE>, hash_gpu_class, eqTrans > elems(25000, hashG, equalG);
+  
+  elems.set_empty_key(empty_key);
+
+  //~ uint64_t hashedId;
+  hashed.resize(1 * NB_HASH_FUNC, 1);
+  hash_id_gpu(hashed, workSpace, size);
+  std::array<int8_t, NODE> id_word;
+  id_word.fill(-1);
+  todo.push_back(&(id_word[0]), NODE);
     
-    Key empty_key(0, empty_word);
+  Key id_key(hashed[0], id_word);
+  elems.insert({ id_key, id_word});
 
-    hash_gpu_class hashG;
-    eqTrans equalG(d_gen, d_words, size, nb_gen, equal);
-    google::dense_hash_map< Key, std::array<int8_t, NODE>, hash_gpu_class, eqTrans > elems(25000, hashG, equalG);
+
+  double timeGpu;
+  auto tstartGpu = high_resolution_clock::now();
+
+  for(int i=0; i<NODE; i++){
+    newtodo.clear();
+    hpcombi_gpu(todo, workSpace, d_gen, hashed, size, NODE, nb_gen, memory);
     
-    elems.set_empty_key(empty_key);
-  
-    //~ uint64_t hashedId;
-    hashed.resize(1 * NB_HASH_FUNC, 1);
-    hash_id_gpu(hashed, workSpace, size);
-    std::array<int8_t, NODE> id_word;
-    id_word.fill(-1);
-    todo.push_back(&(id_word[0]), NODE);
-      
-    Key id_key(hashed[0], id_word);
-    elems.insert({ id_key, id_word});
-  
-  
-    double timeGpu;
-    auto tstartGpu = high_resolution_clock::now();
-  
-    for(int i=0; i<NODE; i++){
-      newtodo.clear();
-      hpcombi_gpu(todo, workSpace, d_gen, hashed, size, NODE, nb_gen, memory);
-      
-      for(int j=0; j<todo.size()/NODE*nb_gen; j++){      
-        std::array<int8_t, NODE> newWord;
-        for(int k=0; k<NODE; k++)
-          newWord[k] = todo.host()[(j/nb_gen)*NODE + k];    
-        newWord[i] = j%nb_gen;
+    for(int j=0; j<todo.size()/NODE*nb_gen; j++){      
+      std::array<int8_t, NODE> newWord;
+      for(int k=0; k<NODE; k++)
+        newWord[k] = todo.host()[(j/nb_gen)*NODE + k];    
+      newWord[i] = j%nb_gen;
+      //~ print_word(newWord);
+      Key new_key(hashed[j * NB_HASH_FUNC], newWord);
+
+      if(elems.insert({ new_key, newWord}).second){
+        newtodo.push_back(&(newWord[0]), NODE);
         //~ print_word(newWord);
-        Key new_key(hashed[j * NB_HASH_FUNC], newWord);
-  
-        if(elems.insert({ new_key, newWord}).second){
-          newtodo.push_back(&(newWord[0]), NODE);
-          //~ print_word(newWord);
-        }
-        else{
-        }
       }
-  
-      todo.swap(&newtodo);
-      cout << i << ", todo = " << todo.size()/NODE << ", elems = " << elems.size()
-           << ", #Bucks = " << elems.bucket_count() << endl;
-      if(todo.size()/NODE == 0)
-        break;
+      else{
+      }
     }
-    
-    auto tfinGpu = high_resolution_clock::now();
-    auto tmGpu = duration_cast<duration<double>>(tfinGpu - tstartGpu);
-    timeGpu = tmGpu.count();
-    printf("Time GPU : %.3f s\n", timeGpu);
-    //~ write_renner(size, nb_gen, elems.size(), timeGpu);
-    
-    cout << "elems =  " << elems.size() << endl;
-    free(gen);
-    free_gen(d_gen);
-    free_words(d_words);
-  //~ }
+
+    todo.swap(&newtodo);
+    cout << i << ", todo = " << todo.size()/NODE << ", elems = " << elems.size()
+         << ", #Bucks = " << elems.bucket_count() << endl;
+    if(todo.size()/NODE == 0)
+      break;
+  }
+  
+  auto tfinGpu = high_resolution_clock::now();
+  auto tmGpu = duration_cast<duration<double>>(tfinGpu - tstartGpu);
+  timeGpu = tmGpu.count();
+  printf("Time GPU : %.3f s\n", timeGpu);
+  //~ write_renner(size, nb_gen, elems.size(), timeGpu);
+  
+  cout << "elems =  " << elems.size() << endl;
+  free(gen);
+  free_gen(d_gen);
+  free_words(d_words);
+//~ }
 }
 
 
